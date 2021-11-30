909ec05fb3502ffb73e087d624f1792f88f39e23
Thomas S <1402736-Thomas-S@users.noreply.gitlab.com>
2021-11-30 15:32:24 +0100

    fix(ui): Add styling with daisyui

--- components/__tests__/header.tsx
+++ components/__tests__/header.tsx
@@ -1,13 +1,22 @@
 import { render } from '@testing-library/preact';
 import Header from '../header';
+import { RecoilRoot } from 'recoil';
 
 describe('Header', () => {
   test('should render correctly on home page', () => {
-    const { asFragment } = render(<Header isHome={true} />);
+    const { asFragment } = render(
+      <RecoilRoot>
+        <Header isHome={true} />
+      </RecoilRoot>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   test('should render correctly on other pages', () => {
-    const { asFragment } = render(<Header isHome={false} />);
+    const { asFragment } = render(
+      <RecoilRoot>
+        <Header isHome={false} />
+      </RecoilRoot>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,45 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { passwordState } from '../containers/sessionProvider';
+import { useRouter } from 'next/router';
 
 interface HeaderProps {
   isHome: boolean;
-  hideLogout?: boolean;
 }
 
-const Header: FC<HeaderProps> = ({ isHome, hideLogout }) => {
-  const setPassword = useSetRecoilState(passwordState);
+const Header: FC<HeaderProps> = ({ isHome }) => {
+  const [password, setPassword] = useRecoilState(passwordState);
+  const router = useRouter();
   return (
     <>
-      {!isHome && (
-        <div>
-          <Link href="/">zurück</Link>
+      <div className="navbar mb-2 shadow-lg bg-primary text-neutral-content rounded-box flex-wrap xs:flex-nowrap">
+        <div className="navbar-start">
+          {!isHome && (
+            <div className="btn btn-outline btn-accent">
+              <Link href="/">zurück</Link>
+            </div>
+          )}
         </div>
-      )}
-      <div>SimpleVolumeControl</div>
-      {!hideLogout && <button onClick={() => setPassword('')}>Logout</button>}
+        <div className="px-2 mx-2 navbar-center w-full xs:w-auto order-first xs:order-none justify-center">
+          <span className="text-lg sm:text-3xl sm:font-thin">
+            SimpleVolumeControl
+          </span>
+        </div>
+        <div className="navbar-end">
+          {password !== '' && (
+            <button
+              onClick={() => {
+                setPassword('');
+                router.push('/login').then();
+              }}
+              className="btn btn-outline btn-accent"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
     </>
   );
 };
