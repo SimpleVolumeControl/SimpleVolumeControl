00c83756f291a793bba8c8d37c1acd014a49b882
Thomas S <1402736-Thomas-S@users.noreply.gitlab.com>
2022-01-10 17:57:25 +0100

    fix(auth): Fix incorrect password handling

--- containers/useAuthenticatedWebsocket.tsx
+++ containers/useAuthenticatedWebsocket.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { passwordState } from './sessionProvider';
 import { useRouter } from 'next/router';
 
 function useAuthenticatedWebsocket(path: string) {
   const didUnmount = useRef(false);
   const [url, setUrl] = useState(() => () => new Promise<string>(() => {}));
-  const password = useRecoilValue(passwordState);
+  const [password, setPassword] = useRecoilState(passwordState);
   const router = useRouter();
   useEffect(() => {
     const { protocol, host } = window.location;
@@ -29,9 +29,10 @@ function useAuthenticatedWebsocket(path: string) {
   }, [sendMessage, password]);
   useEffect(() => {
     if (lastMessage?.data?.startsWith('DEAUTH:')) {
+      setPassword('');
       router.push('/login').then();
     }
-  }, [lastMessage, router]);
+  }, [lastMessage, router, setPassword]);
   return { lastMessage, sendMessage };
 }
 
